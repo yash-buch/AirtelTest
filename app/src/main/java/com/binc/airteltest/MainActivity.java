7624df1ccb061e51ffe23b0b4c67a95ef0c6e4ef
Yash <yash@Yashs-MacBook-Pro.local>
2020-07-16 22:00:43 +0530

    initial commit

---
+++ app/src/main/java/com/binc/airteltest/MainActivity.java
@@ -0,0 +1,129 @@
+package com.binc.airteltest;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static androidx.recyclerview.widget.LinearLayoutManager.VERTICAL;
+
+public class MainActivity extends AppCompatActivity {
+    private static String TAG = MainActivity.class.getName();
+
+    int screenWidth;
+    int screenHeight;
+    int baseWidth = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        screenHeight = dm.heightPixels;
+        screenWidth = dm.widthPixels;
+        Log.i(TAG, "screen width: "+screenWidth);
+        RecyclerView rv = findViewById(R.id.rv);
+        LinearLayoutManager lm = new LinearLayoutManager(this, VERTICAL, false);
+        rv.setLayoutManager(lm);
+        final CustomAdapter adapter = new CustomAdapter(getRVItemPairs(3));
+        rv.setAdapter(adapter);
+        final EditText et = findViewById(R.id.editText);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = et.getText().toString();
+                try {
+                    adapter.rows = getRVItemPairs(Integer.parseInt(str));
+                } catch (NumberFormatException e) {
+                    adapter.rows = getRVItemPairs(3);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private List<Row> getRVItemPairs(int total) {
+        int frameNum = 1;
+        List<Row> rows = new ArrayList<>();
+        while(frameNum <= total) {
+            int sw = screenWidth;
+            Row row = new Row();
+            row.viewList = new ArrayList<>();
+            while(sw >= 0) {
+                int frameWidth = baseWidth * frameNum;
+                if (frameWidth > sw || frameNum > total) {
+                    rows.add(row);
+                    break;
+                }
+                View v = new View(MainActivity.this);
+                v.setLayoutParams(new LinearLayout.LayoutParams(frameWidth, screenHeight/10));
+                row.viewList.add(v);
+                sw -= frameWidth;
+                frameNum++;
+            }
+        }
+        return rows;
+    }
+
+    class Row {
+        List<View> viewList;
+    }
+
+    class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder> {
+
+        List<Row> rows;
+        public CustomAdapter(List<Row> rows) {
+            this.rows = rows;
+        }
+
+        @NonNull
+        @Override
+        public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LinearLayout ll = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+            return new CustomViewHolder(ll);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+            List<View> views = rows.get(position).viewList;
+            for(int i = 0; i < views.size(); i++) {
+                View v = views.get(i);
+                if(i%2 == 0) {
+                    v.setBackgroundColor(Color.BLACK);
+                } else {
+                    v.setBackgroundColor(Color.WHITE);
+                }
+                holder.ll.addView(v);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return rows.size();
+        }
+    }
+
+    static class CustomViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout ll;
+        public CustomViewHolder(@NonNull LinearLayout itemView) {
+            super(itemView);
+            ll = itemView;
+        }
+    }
+}
